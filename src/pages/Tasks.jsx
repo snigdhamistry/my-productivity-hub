fd6e4e0914653c15c44912cc9158b871d57e20a6
Snigdha Mistry <150170898+snigdhamistry@users.noreply.github.com>
2026-02-24 22:58:02 +0530

    implement delete task using filter and index

--- src/pages/Tasks.jsx
+++ src/pages/Tasks.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react'
+import { Trash2 } from 'lucide-react';
 
 const Tasks = () => {
        const [text, setText] = useState('')
@@ -17,24 +18,30 @@ const Tasks = () => {
                      setText('')
               }
        }
-
        return (
+              <div className='bg-black h-screen w-screen flex flex-col items-center justify-start gap-5 pt-10' >
+                     <h1 className='text-2xl font-bold text-amber-50'> My Tasks</h1>
 
-              <>
-                     <h1>My Tasks</h1>
+                     <input type="text" placeholder='Type your task here' value={text} maxLength={50} className='bg-white' onChange={(e) => { setText(e.target.value) }} />
 
-                     <input onChange={(e) => { setText(e.target.value) }} type="text" placeholder='Type your task here' value={text} maxLength={50} />
-
-                     <button onClick={addTask} >Add task</button>
+                     <button onClick={addTask} className='bg-amber-500'>Add task</button>
                      {error && <p className='text-red-800'>{error}</p>}
                      <div>
                             {task.map((elem, idx) => {
-                                   return <div key={idx}>{idx + 1}
-                                          <input type="checkbox" name="list" />{elem}
+                                   return <div key={idx} className='text-white flex items-center gap-2'>
+                                          {/* {idx + 1} */}
+                                          <input type="checkbox" name="list" className="ml-2 bg-amber-800" />{elem}
+                                          <button onClick={() => {
+                                                 setTask(task.filter((elem, idx2) => {
+                                                        return idx2 !== idx
+                                                 }))
+                                          }}>
+                                                 <Trash2 />
+                                          </button>
                                    </div>
                             })}
                      </div>
-              </>
+              </div >
        )
 }
 
