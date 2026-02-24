b3bbd1043a6642db9519c17ca1d8b646a48ac156
Snigdha Mistry <150170898+snigdhamistry@users.noreply.github.com>
2026-02-24 12:39:57 +0530

    adding inpput,button and task checkbox

--- src/pages/Tasks.jsx
+++ src/pages/Tasks.jsx
@@ -1,11 +1,31 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Tasks = () => {
+       const [text, setText] = useState('')
+       const [task, setTask] = useState([])
+       const addTask = () => {
+              setTask([...task, text])
+              setText('')
+       }
+
        return (
-              <div>
-                     <h1>Tasks Page</h1>
-              </div>
+
+              <>
+                     <h1>My Tasks</h1>
+
+                     <input onChange={(e) => { setText(e.target.value) }} type="text" placeholder='Type your task here' value={text} maxLength={50} />
+
+                     <button onClick={addTask} >Add task</button>
+
+                     <div>
+                            {task.map((elem, idx) => {
+                                   return <div key={idx}>{idx + 1}
+                                          <input type="checkbox" name="list" />{elem}
+                                   </div>
+                            })}
+                     </div>
+              </>
        )
 }
 
-export default Tasks
+export default Tasks 
