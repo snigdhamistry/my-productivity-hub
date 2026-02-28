70f44d14865e6d825168c4f2d4acb37f2f346672
Snigdha Mistry <150170898+snigdhamistry@users.noreply.github.com>
2026-02-28 22:43:22 +0530

    create basic weather page layout with input field and search button

--- src/pages/Weather.jsx
+++ src/pages/Weather.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
+import { Search } from 'lucide-react';
 
 const Weather = () => {
        return (
-              <div>
-                     <h1>Weather Page</h1>
+              <div className='bg-black h-screen w-screen flex flex-col items-center justify-start gap-5 pt-10  text-amber-50' >
+                     <h1 className='text-3xl font-bold'>Weather Page</h1>
+                     <input type="text" placeholder="Enter city name" className="bg-gray-500 text-white p-2 rounded-md" />
+                     <button className="bg-amber-500 text-black p-2 rounded-md flex items-center gap-2 ">Get Weather
+                            <Search />
+                     </button>
               </div>
        )
 }
