63a456f8d1ffb03584a2e6682d50bbc52a34525d
Snigdha Mistry <150170898+snigdhamistry@users.noreply.github.com>
2026-03-02 14:39:23 +0530

    change the name of handlers

--- src/pages/Weather.jsx
+++ src/pages/Weather.jsx
@@ -4,13 +4,13 @@ import { Search } from 'lucide-react';
 const Weather = () => {
        const [city, setCity] = useState('')
        const [error, setError] = useState('')
-       const handleSearch = (e) => {
+       const handleFormSubmit = (e) => {
               e.preventDefault()
        }
-       const typeCity = (e) => {
+       const handleInputChange = (e) => {
               setCity(e.target.value)
        }
-       const handleCityChange = () => {
+       const handleGetWeather = () => {
               if (city.trim() === '') {
                      setError('please enter a city name')
                      setCity('')
@@ -25,12 +25,12 @@ const Weather = () => {
        return (
               <div className='bg-black h-screen w-screen flex flex-col items-center justify-start gap-5 pt-10  text-amber-50' >
 
-                     <form onSubmit={handleSearch} className='flex flex-col items-center gap-5'>
+                     <form onSubmit={handleFormSubmit} className='flex flex-col items-center gap-5'>
                             <h1 className='text-3xl font-bold'>Weather Page</h1>
 
-                            <input type="text" placeholder="Enter city name" onChange={typeCity} value={city} className="bg-gray-500 text-white p-2 rounded-md" />
+                            <input type="text" placeholder="Enter city name" onChange={handleInputChange} value={city} className="bg-gray-500 text-white p-2 rounded-md" />
 
-                            <button onClick={handleCityChange} className="bg-amber-500 text-black p-2 rounded-md flex items-center gap-2 ">Get Weather
+                            <button onClick={handleGetWeather} className="bg-amber-500 text-black p-2 rounded-md flex items-center gap-2 ">Get Weather
                                    <Search />
                             </button>
                             
