174ed52746e2aa226d51a4323a3ed765f38c7e80
Snigdha Mistry <150170898+snigdhamistry@users.noreply.github.com>
2026-03-05 15:41:12 +0530

    add profile page and theme component and update routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,23 @@
 import React from 'react'
-import Navbar from './components/Navbar'
 import { Route, Routes } from 'react-router-dom'
+import Navbar from './components/Navbar/Navbar'
+import Theme from './components/Theme'
 import Home from './pages/Home'
 import Notes from './pages/Notes'
 import Tasks from './pages/Tasks'
 import Weather from './pages/Weather'
+import Profile from './pages/Profile'
 const App = () => {
   return (
     <div>
-      <Navbar />
+      <Navbar/>
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/notes' element={<Notes />} />
         <Route path='/tasks' element={<Tasks />} />
         <Route path='/weather' element={<Weather />} />
+        <Route path='/profile' element={<Profile />} />
+        <Route path='/theme' element={<Theme />} />
       </Routes>
     </div>
   )
---
+++ src/pages/Profile.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+const Profile = () => {
+       return (
+              <>
+              <Link to="/theme">You can change theme from here Theme</Link>
+              </>
+       )
+}
+
+export default Profile
