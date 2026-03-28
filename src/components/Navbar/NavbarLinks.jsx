ef21cb27ca24229a16b1489e478bd746ca7fca81
Snigdha Mistry <150170898+snigdhamistry@users.noreply.github.com>
2026-03-28 20:47:42 +0530

    Changes to notes section and added links to navbar

--- src/components/Navbar/NavbarLinks.jsx
+++ src/components/Navbar/NavbarLinks.jsx
@@ -26,6 +26,13 @@ const NavbarLinks = () => {
                             Tasks
                      </Link>
 
+                     <Link
+                            to='https://github.com/snigdhamistry/my-productivity-hub'
+                            className='relative text-white text-lg font-medium hover:text-yellow-400 transition duration-300 after:absolute after:left-0 after:-bottom-1 after:h-0.5 after:w-0 after:bg-yellow-400 after:transition-all after:duration-300 hover:after:w-full'
+                     >
+                            Source Code
+                     </Link>
+
                      
 
               </div>
--- src/pages/Notes.jsx
+++ src/pages/Notes.jsx
@@ -1,10 +1,24 @@
 import React from 'react'
-
+import { useState } from 'react';
 const Notes = () => {
+       const [input, setInput] = useState('')
+       const [text, setText] = useState('')
+
        return (
-              <div>
-                     <h1 className='text-4xl font-bold text-black'>Notes Page</h1>
-                     <h1 className='text-2xl text-black'>Comming soon.............</h1>
+              <div className="h-screen bg-gray-900 flex justify-center pt-28 pb-9 px-4 text-white overflow-hidden">
+                     <form className="w-full max-w-2/3 bg-gray-800 p-5 rounded-xl shadow-xl flex flex-col gap-5">
+                            <h1 className="text-3xl font-bold text-center text-amber-400 tracking-wide">
+                                   My Notes
+                            </h1>
+
+                            <input value={input} onChange={(e) => setInput(e.target.value)} type="text" placeholder='type your title here...' maxLength="100" className="bg-gray-800 text-white placeholder:text-gray-500 border border-gray-600 focus:outline-none focus:ring-2 focus:ring-blue-500" />
+
+                            <textarea onChange={(e)=>{setText(e.target.value)}} value={text} className="bg-gray-800 text-white placeholder:text-gray-500 border border-gray-600 focus:outline-none focus:ring-2 focus:ring-blue-500" placeholder="Write your note here..." />
+
+                            <button type="submit" className="bg-amber-500 hover:bg-amber-600 text-black font-bold py-2 px-4 rounded-lg transition duration-300">
+                                   Save Note
+                            </button>
+                     </form>
               </div>
        )
 }
